071012cea2a5aca14424ed5c02f9a3e8945eb8bc
Krisly <linder2411@gmail.com>
2018-02-19 16:14:39 +0100

    krisly

---
+++ install_panelcheck.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+sudo mkdir /Applications/PanelCheck                   
+cd /Applications/PanelCheck                       
+curl 'https://github.com/CPHFOOD/PanelCheck/blob/master/PanelCheck_V1.4.2_sourceCode.zip' 
+sudo unzip PanelCheck_V1.4.2_sourceCode.zip                           
+sudo rm PanelCheck_V1.4.2_sourceCode.zip
+
+OUTPUT=$(which pip)
+if echo "$OUTPUT" | grep -q "pip"; then
+    echo "Pip is already installed, type password to continue"
+else 
+	echo "Pip not installed, installing pip"
+	sudo rm -f /usr/bin/easy_install*
+	sudo rm -f /usr/local/bin/easy_install*
+	curl -O https://svn.apache.org/repos/asf/oodt/tools/oodtsite.publisher/trunk/distribute_setup.py
+	sudo python distribute_setup.py
+	sudo rm distribute_setup.py
+	sudo easy_install pip
+fi
+
+
+pip2 install -U wxPython
+pip2 install matplotlib numpy scipy pandas xlrd rpy2 
+sudo chmod u+x /Applications/PanelCheck/PanelCheck_V1.4.2_sourceCode/run_panelcheck.sh
+ln -s /Applications/PanelCheck/PanelCheck_V1.4.2_sourceCode/run_panelcheck.sh $HOME/Desktop/
+
